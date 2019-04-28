5ebc565a5edd17a2f7a3aad151d5e3530b43fb2d
Raman Gupta <ramanxg@gmail.com>
2019-04-27 19:46:21 -0700

    pass through

--- static/google.js
+++ static/google.js
@@ -26,7 +26,7 @@ function attachSignin(element) {
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   var em = profile.getEmail();
-  var id_token = googleUser.getAuthResponse().id_token;
+  var token = googleUser.getAuthResponse().id_token;
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
