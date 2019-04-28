d254444727d52ed881d9a5315ce5f5e0edde3dc6
Raman Gupta <ramanxg@gmail.com>
2019-04-27 19:37:55 -0700

    pass through

--- static/google.js
+++ static/google.js
@@ -26,10 +26,11 @@ function attachSignin(element) {
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   var token = googleUser.getAuthResponse().id_token;
+  var em = profile.getEmail();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  $.post('/', { email: profile.getEmail(), id_token: token }, function(data) {
+  $.post('/', { email: em, id_token: token }, function(data) {
     console.log("PRINTING OUT POST DATA");
     console.log(data);
   });
