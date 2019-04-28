3b5cf396dd15ccb7e12bf46752114502356a8c65
Raman Gupta <ramanxg@gmail.com>
2019-04-27 18:06:05 -0700

    pass through

--- static/code.js
+++ static/code.js
@@ -11,7 +11,7 @@ function initMap() {
     streetViewControl: false
   });
   infoWindow = new google.maps.InfoWindow;
-  
+  addUser();
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
