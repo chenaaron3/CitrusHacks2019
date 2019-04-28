60686b567f08ecae42afb2c0d80fabd7e0ad0fe6
Raman Gupta <ramanxg@gmail.com>
2019-04-27 19:41:39 -0700

    pass through

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
 	if request.method == 'POST':
-		userp = request.data.user
+		email = request.args.get('email')
+		id_token = request.args.get('id_token')
 		app.logger.info('testing info log')
 		time.sleep(10);
-		return redirect(url_for('map', googleUser=userp))
+		return redirect(url_for('map', email=email, id_token=id_token))
 	return render_template("index.html")
 
 @app.route('/map', methods=["GET", "POST"])
 def map():
-    return render_template("map.html", googleUser=request.args.get('user'))
+    return render_template("map.html", email=request.args.get('email'), id_token=id_token('id_token'))
 
 if __name__ == "__main__":
     app.run(debug=True)	
--- static/google.js
+++ static/google.js
@@ -25,10 +25,10 @@ function attachSignin(element) {
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  var token = googleUser.getAuthResponse().id_token;
-  var em = profile.getEmail();
+  var id_token = googleUser.getAuthResponse().id_token;
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   $.post('/', { email: em, id_token: token }, function(data) {
     console.log("PRINTING OUT POST DATA");
