3c642a42b8423390843564370d7d78476db386d8
Raman Gupta <ramanxg@gmail.com>
2019-04-27 19:54:40 -0700

    kms

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-	if request.method == 'POST':
+	"""if request.method == 'POST':
 		email = request.args.get('email')
 		id_token = request.args.get('id_token')
 		time.sleep(10);
-		return redirect(url_for('map', email=email, id_token=id_token))
+		return redirect(url_for('map', email=email, id_token=id_token))"""
 	return render_template("index.html")
 
 @app.route('/map', methods=["GET", "POST"])
 def map():
-    return render_template("map.html", email=request.args.get('email'), id_token=request.args.get('id_token'))
+    return render_template("map.html")#, email=request.args.get('email'), id_token=request.args.get('id_token'))
 
 if __name__ == "__main__":
     app.run(debug=True)	
